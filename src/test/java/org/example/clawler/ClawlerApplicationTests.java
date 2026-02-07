ed7f7cddfcdc66e9aac966cccfe281996056cdf6
Evgeni-Kr <krotevgeni1@gmail.com>
2026-02-07 13:28:18 +0300

    Initial commit

---
+++ src/test/java/org/example/clawler/ClawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
